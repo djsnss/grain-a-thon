1663caba9124f4e3f59a0387a98b4a7b32aae5e5
Vir Kothari <virkothari8@gmail.com>
2024-10-14 23:30:26 +0530

    Add pages for each dept

---
+++ src/components/VolunteerInfo.tsx
@@ -0,0 +1,28 @@
+import './VolunteerInfo.css'
+
+export default function VolunteerInfo() {
+  return (
+    <div className="volunteers-container w-full flex flex-col justify-center grow">
+      <div className="volunteer-info flex justify-around p-3.5">
+        <div className="volunteer-name">B C K Z G V</div>
+        <div className="volunteer-name">2 6  K G S</div>
+      </div>
+      <div className="volunteer-info flex justify-around p-3.5">
+        <div className="volunteer-name">B C K Z G V</div>
+        <div className="volunteer-name">2 6  K G S</div>
+      </div>
+      <div className="volunteer-info flex justify-around p-3.5">
+        <div className="volunteer-name">B C K Z G V</div>
+        <div className="volunteer-name">2 6  K G S</div>
+      </div>
+      <div className="volunteer-info flex justify-around p-3.5">
+        <div className="volunteer-name">B C K Z G V</div>
+        <div className="volunteer-name">2 6  K G S</div>
+      </div>
+      <div className="volunteer-info flex justify-around p-3.5">
+        <div className="volunteer-name">B C K Z G V</div>
+        <div className="volunteer-name">2 6  K G S</div>
+      </div>
+    </div>
+  )
+}
