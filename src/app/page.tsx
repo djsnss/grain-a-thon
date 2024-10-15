42f70675e2dfc90f6123a46aec1aef3cc6333968
tanishshah <tanishshah20@gmail.com>
2024-10-16 05:17:21 +0530

    leaderboard page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import NSSLogo from "./DJSNSSLogo.png"; // Example image
 import axios from "axios";
+import Link from "next/link";
 
 const ProgressCarousel = ({ progressData }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -16,7 +17,7 @@ const ProgressCarousel = ({ progressData }) => {
   }, [progressData.length]);
 
   return (
-    <div className="w-full flex justify-center items-center overflow-hidden">
+    <div className="h-max w-[500px] flex justify-center items-center overflow-hidden">
       <div className="relative w-full max-w-4xl">
         <AnimatePresence>
           <motion.div
@@ -27,17 +28,18 @@ const ProgressCarousel = ({ progressData }) => {
             className="relative w-full h-[200px] md:h-[300px] lg:h-[400px] rounded-xl overflow-hidden"
           >
             <Image
-              src={NSSLogo}
+              src={progressData[currentIndex].img}
               alt={progressData[currentIndex]._id}
               layout="fill"
-              objectFit="cover"
-              className="rounded-xl"
+              objectFit=""
+              className="rounded-xl scale-90"
             />
-            <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col justify-center items-center text-center">
-              <h2 className="text-white text-3xl sm:text-4xl md:text-5xl font-extrabold tracking-wider">
+            <div className="absolute inset-0 flex flex-col justify-end items-start p-8 md:p-12"
+              style={{ background: `linear-gradient(to top, ${progressData[currentIndex].color}, transparent)`}}>
+              <Link href={progressData[currentIndex].link} className="text-white text-3xl md:text-5xl font-extrabold tracking-wider mb-2 drop-shadow-lg">
                 {progressData[currentIndex]._id}
-              </h2>
-              <p className="text-white text-xl sm:text-2xl md:text-3xl font-semibold mt-2">
+              </Link>
+              <p className="text-white text-xl md:text-3xl font-semibold mt-2 drop-shadow-md">
                 {progressData[currentIndex].totalGrainCollected} Kgs
               </p>
             </div>
@@ -52,14 +54,14 @@ export default function Home() {
   const [progressData, setProgressData] = useState([]);
 
   let progressDataTest = [
-    { _id: 'COMPS', totalGrainCollected: 100, color: '#f94144' },
-    {  _id: 'IT', totalGrainCollected: 80, color: '#f3722c' },
-    {  _id: 'CSDS', totalGrainCollected: 40, color: '#f8961e' },
-    {  _id: 'AIML', totalGrainCollected: 90, color: '#f9c74f' },
-    {  _id: 'AIDS', totalGrainCollected: 65, color: '#90be6d' },
-    {  _id: 'ICB', totalGrainCollected: 45, color: '#43aa8b' },
-    {  _id: 'EXTC', totalGrainCollected: 55, color: '#4d908e' },
-    {  _id: 'MECH', totalGrainCollected: 55, color: '#577590' },
+    { _id: 'COMPS', totalGrainCollected: 100, color: '#f94144', img:'/images/th 9.jpg', link: "/branches/comps" },
+    {  _id: 'IT', totalGrainCollected: 80, color: '#f3722c', img:'/images/th 10.jpg', link: "/branches/it" },
+    {  _id: 'CSDS', totalGrainCollected: 40, color: '#f8961e', img:'/images/th 11.jpg', link: "/branches/csds" },
+    {  _id: 'AIML', totalGrainCollected: 90, color: '#f9c74f', img:'/images/th 12.jpg', link: "/branches/aiml" },
+    {  _id: 'AIDS', totalGrainCollected: 65, color: '#90be6d', img:'/images/th 13.jpg', link: "/branches/aids" },
+    {  _id: 'ICB', totalGrainCollected: 45, color: '#43aa8b', img:'/images/th 14.jpg', link: "/branches/icb" },
+    {  _id: 'EXTC', totalGrainCollected: 55, color: '#4d908e', img:'/images/th 15.jpg', link: "/branches/extc" },
+    {  _id: 'MECH', totalGrainCollected: 55, color: '#577590', img:'/images/th 16.jpg', link: "/branches/mech" },
   ];
 
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,8 +21,8 @@ export function FloatingNavDemo() {
         { name: "AIML", link: "/branches/aiml" },
         { name: "ICB", link: "/branches/icb" },
         { name: "MECH", link: "/branches/mech" },
-        { name: "ELEC", link: "/branches/extc" },
-        { name: "CS-DS", link: "/branches/csds" },
+        { name: "EXTC", link: "/branches/extc" },
+        { name: "CSDS", link: "/branches/csds" },
       ],
     },
     {
